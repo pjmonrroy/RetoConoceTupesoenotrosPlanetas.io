d90ce515ca807a5b95f9c2e876f614a481a27021
pjmonrroy <pierremonrroy@yahoo.com>
2022-04-30 18:40:50 -0500

    version2.1

--- formula-jupiter.js
+++ formula-jupiter.js
@@ -1,7 +1,7 @@
 var usuario = prompt("Cual es tu peso kg "); // es un valor en 
 // var planeta = prompt("Elige tu planeta.1 es marte y 2 es jupiter");
 var planeta = parseInt(prompt("Elige tu planeta.\n1 es Mercurio,  \n2 es Venus, \n3 es La tierra, \n4 Marte, \n5 es Jupiter, \n6 es Saturno, \n7 es Urano, \n8 es Neptuno")); 
-var peso = parseInt(usuario)
+var peso = parseFloat(usuario)
 var g_mercurio = 3.7;
 var g_venus = 8.87;
 var g_tierra = 9.8;
@@ -13,7 +13,7 @@ var g_neptuno = 11.15;
 var peso_final;
 if ( planeta == 1) {
     peso_final = peso * g_mercurio / g_tierra;
-    console.log(peso_final = parseInt(peso_final))
+    console.log(peso_final = (peso_final.toFixed(2)))
     console.log(document.write("Tu peso en Mercurio es: <strong> " + peso_final + " kilos</strong>"));
     
 }
